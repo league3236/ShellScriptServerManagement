6abd1b9f024a8041fb3775174fcb55a38a909326
KwakSeungHyeok <league3236@gmail.com>
2019-11-13 05:13:27 +0000

    Create ansibleCoreInstall_pip3.sh

---
+++ ansibleCoreInstall_pip3.sh
@@ -0,0 +1,11 @@
+# !/bin/bash
+
+#hostnamectl set-hostname m01
+#systemctl restart network
+#hostname
+
+########pip3 install
+pip3 install ansible
+ansible --version
+
+ansible --version | grep "python version"
